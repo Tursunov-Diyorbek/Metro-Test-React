17df7adfb6ba2887e9c40ccb2980eaa198e04f74
Ayubiy1 <ayubiyurinboyev@gmail.com>
2024-01-25 17:19:17 +0500

    test work

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router";
-import HomePage from "../components/homePage";
+import Home from "../components/homePage";
 import LoginPage from "../components/login";
 import styles from "./index.module.sass";
 import "./App.css";
+import HomePage from "../components/homePage/home";
+import TestWork from "../components/test-work";
 
 function App() {
   const [logo, setLogo] = useState(true);
@@ -23,10 +25,15 @@ function App() {
                 <img src="/logo2.png" alt="logo" />
               </div>
             ) : (
-              <HomePage />
+              <Home />
             )
           }
-        />
+        >
+          <Route path="/" element={<HomePage />} />
+        </Route>
+
+        <Route path="test-work/:id" element={<TestWork />} />
+
         <Route path="/auth/login" element={<LoginPage />} />
       </Routes>
     </div>
--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -1,33 +1,16 @@
 import React, { useState } from "react";
-import styles from "./index.module.sass";
 import { FaTelegram } from "react-icons/fa";
 import Navbar from "../navbar";
+import { Outlet } from "react-router";
+import styles from "./index.module.sass"
 
-export default function HomePage() {
-
+export default function Home() {
   return (
     <>
       <Navbar />
-      <div className={styles.img}>
-        <img src="/Alisher Navoiy 1.jpeg" alt="img" />
-        <div className={styles.img__filter}></div>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <h2>
-            Metropoliten xodimlari malakasini baholash tizimiga xush kelibsiz
-          </h2>
-        </div>
-      </div>
-      <div className={styles.footer}>
-        <h3>MetroTest</h3>
-        <a href="https://t.me/Feyzr_554" target="_tblank">
-          <FaTelegram /> {"Biz bilan bo'glanish"}
-        </a>
+
+      <div className={styles.containet}>
+        <Outlet />
       </div>
     </>
   );
